d88311f9a9429b5ff01ee7bdd9843fbb0a3246f6
nityachy <93797446+nityachy@users.noreply.github.com>
2021-12-14 23:54:57 -0500

    Add files via upload

---
+++ main/duties.js
@@ -0,0 +1,48 @@
+///Global variables///
+const cookie_name = 'balance';
+var cur_balance=0;
+var earnings = "";
+
+/// Display all necessary settings and values at the beginning of the game ///
+function begin(){
+    //Initial balance
+    var cval = get_cookie(cookie_name);
+	cur_balance = parseInt(cval);
+    update_display(cur_balance);
+}
+
+/// Update balance when user clicks ///
+function increment_and_update(name, earned_amt){
+	cur_balance+=parseInt(earned_amt);
+    set_cookie(cookie_name, cur_balance);
+	earnings=name+"; "+earnings;
+    update_display(cur_balance);
+}
+
+function update_display(cval){
+	var b_display = document.getElementById('balance_display');
+	var h_balance = document.getElementById('hidden_balance');
+	var h_earnings = document.getElementById('hidden_earnings');
+    b_display.innerHTML = "Balance: $"+ cval;
+    console.log(cval);
+    h_balance.value = cval;
+	h_earnings.value = earnings;
+}
+
+/* Cookie-handling functions */
+
+/// Retrieve the value of a cookie ///
+function get_cookie(cookie_name){
+    var decodedCookie = decodeURIComponent(document.cookie);
+    return decodedCookie
+        .split('; ')
+        .find(row => row.startsWith(`${cookie_name}=`))
+        .split('=')[1];
+}
+
+/// Set the new value for a cookie ///
+function set_cookie(cookie_name, cval){
+    document.cookie = `${cookie_name}=${cval}`;
+}
+
+begin();
